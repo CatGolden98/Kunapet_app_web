44fc8abd0e73bc69307113bafc7923f0d8df70a3
luis hernandez <axxate@gmail.com>
2025-10-17 20:10:51 -0500

    chore: setup new repo deploy; feat: supabase fallback + base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // GitHub Pages (proyecto): https://CatGolden98.github.io/Kunapet_app/
-  base: "/Kunapet_app/",
+  base: "/Kunapet_app_web/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
